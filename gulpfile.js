226e4741cdc9eb6a704f9ec4df968203494771fa
Fernando Gonzalez <fernandogprepositorio@gmail.com>
2023-10-12 23:02:59 -0600

    Agrega browserList y exportdefault series

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer')
 
 function css(done){
   //Compilar sass
   src('src/scss/app.scss')
     .pipe( sass() )
+    .pipe( postcss( [ autoprefixer() ] ) )
     .pipe( dest('build/css') )
 
     done();
@@ -16,3 +19,7 @@ function dev(){
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series ( css,dev )
+
+//series - Se inicia una tarea y hasta que finaliza inicia la siguiente
+// parallel - Todas inician al mismo tiempo
