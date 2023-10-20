6cfa8503219b64cb9fa73f22b054b19c91cfa396
Fernando Gonzalez <fernandogprepositorio@gmail.com>
2023-10-20 16:37:02 -0600

    Se agrega dependencia avif

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
+
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif')
 
 function css(done){
   //Compilar sass
@@ -13,14 +17,40 @@ function css(done){
     done();
 }
 
+function imagenes (done){
+  src('src/img/**/*')
+    .pipe( imagemin({optimizationLevel: 3}) )
+    .pipe( dest('build/img') );
+    done();
+}
+
+function versionWebp(){
+  return src('src/img/**/*.{png,jpg}')
+    .pipe( webp() )
+    .pipe(dest('build/img'));
+}
+
+function versionAvif(){
+  const opciones = {
+    quality: 50
+  }
+  return src('src/img/**/*.{png,jpg}')
+    .pipe( avif( opciones ) )
+    .pipe(dest('build/img'))
+}
+
 function dev(){
   watch( 'src/scss/**/*.scss', css);
   //watch('src/scss/app.scss', css);
+  watch( 'src/img/**/*', imagenes );
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series ( css,dev )
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp
+exports.versionAvif = versionAvif;
+exports.default = series ( imagenes, versionWebp, versionAvif, css, dev )
 
 //series - Se inicia una tarea y hasta que finaliza inicia la siguiente
 // parallel - Todas inician al mismo tiempo
